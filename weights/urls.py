cf6008a38037e21e4f999490119b4e20e0dc8e7e
lukewtom93 <lukewtom93@gmail.com>
2025-04-22 04:52:28 +0100

    file structure organized into frontend and backend

--- weights/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from weights import views
-
-
-urlpatterns = [
-    path('weights/', views.WeightEntryList.as_view()),
-    path('weights/<int:pk>/', views.WeightEntryDetail.as_view()),
-]
