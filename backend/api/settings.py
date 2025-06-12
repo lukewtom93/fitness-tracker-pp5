288131c2031777ca3d90d5bbf628284c1d712fb0
lukewtom93 <lukewtom93@gmail.com>
2025-06-12 16:33:58 +0100

    staticfiles added

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "frontend" / "build" / "static",
